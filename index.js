953b77e09102485d0fada3df7914ca997c27cf28
VTFStacy <stacycoomber@hotmail.co.uk>
2020-05-01 18:51:40 +0100

    tweaker the header

--- index.js
+++ index.js
@@ -1,29 +1,101 @@
 $(document).ready(function(){
-var hovered = false;
-    $("#button-about-us").mouseenter(function(){
-        
-        $("#about").fadeIn('slow');
+
+
+
+    $("#button-about").mouseenter(function(){
+    var hovered = false;  
+        $("#about").fadeIn(500);
+        $("#button-about").css("background-color","rgba(255,205,1)");
 
         $ ("#about").mouseenter(function(){
             hovered = true;
-            console.log(hovered);
+           
         });
             $("#about").mouseleave(function() {
              setTimeout(function() {
-            $("#about").fadeOut('slow');
+            $("#about").fadeOut(100);
+             $("#button-about").css("background-color","white");
+            hovered = false;
+            },1000);
+            });
+
+        $("#button-about").mouseleave(function(){
+           
+            setTimeout(function(){
+                if (hovered === false) {
+                    console.log("false hover");
+                    $("#about").fadeOut(100);
+                     $("#button-about").css("background-color","white");
+                }
+            },500);
+        });
+    
+    });
+
+
+    $("#button-outreach").mouseenter(function(){
+    var hovered = false;  
+        $("#outreach").fadeIn(500);
+        $("#button-outreach").css("background-color","rgb(255,205,1)");
+
+
+        $ ("#outreach").mouseenter(function(){
+            hovered = true;
+         
+        });
+            $("#outreach").mouseleave(function() {
+             setTimeout(function() {
+            $("#outreach").fadeOut(100);
+              $("#button-outreach").css("background-color","white");
+
             hovered = false;
             },500);
             });
 
-        $("#button-about-us").mouseleave(function(){
-            console.log("left " + hovered);
+        $("#button-outreach").mouseleave(function(){
+           
             setTimeout(function(){
                 if (hovered === false) {
                     console.log("false hover");
-                    $("#about").fadeOut('slow');
+                    $("#outreach").fadeOut(100);
+                      $("#button-outreach").css("background-color","white");
+
                 }
             },1000);
         });
     
     });
+
+    /*need to figure out how to pass arguments that select dom into jquery */
+//WIP
+// var NavBarred = function(item){
+//     $("#button-"+item).mouseenter(function(){
+//     var hovered = false;  
+//         $("#"+ item).fadeIn('slow');
+
+//         $ ("#" +item).mouseenter(function(){
+//             hovered = true;
+//             console.log(hovered);
+//         });
+//             $("#" +item).mouseleave(function() {
+//              setTimeout(function() {
+//             $("#"+item).fadeOut('slow');
+//             hovered = false;
+//             },500);
+//             });
+
+//         $("#button-" +item).mouseleave(function(){
+//             console.log("left " + hovered);
+//             setTimeout(function(){
+//                 if (hovered === false) {
+//                     console.log("false hover");
+//                     $("#"+item).fadeOut('slow');
+//                 }
+//             },1000);
+//         });
+    
+//     });
+// };
+// NavBarred(outreach);
+
 });
