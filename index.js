d678193773a79b05152bfe9f9dde5cf49578c99f
VTFStacy <stacycoomber@hotmail.co.uk>
2020-05-02 12:56:34 +0100

    added slideshow
    updated the banner - looks maybe too 'artsy' now..

--- index.js
+++ index.js
@@ -1,101 +1,50 @@
 $(document).ready(function(){
 
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
 
 
-    $("#button-about").mouseenter(function(){
-    var hovered = false;  
-        $("#about").fadeIn(500);
-        $("#button-about").css("background-color","rgba(255,205,1)");
-
-        $ ("#about").mouseenter(function(){
-            hovered = true;
-           
-        });
-            $("#about").mouseleave(function() {
-             setTimeout(function() {
-            $("#about").fadeOut(100);
-             $("#button-about").css("background-color","white");
-            hovered = false;
-            },1000);
-            });
-
-        $("#button-about").mouseleave(function(){
-           
-            setTimeout(function(){
-                if (hovered === false) {
-                    console.log("false hover");
-                    $("#about").fadeOut(100);
-                     $("#button-about").css("background-color","white");
-                }
-            },500);
-        });
-    
-    });
+// var num = 0
+// function slideShow() {
+// var imageDOM = document.querySelector('#slide');
+// var nImageDOM = document.querySelector('#slide2');
+// console.log(num);
 
 
-    $("#button-outreach").mouseenter(function(){
-    var hovered = false;  
-        $("#outreach").fadeIn(500);
-        $("#button-outreach").css("background-color","rgb(255,205,1)");
+// if ( num === 1 ) {
+//     imageDOM.src = "images/art-therapy-2.jpeg";
+// } 
+// if ( num === 2 ) {
+//     imageDOM.src = "images/art-therapy-3.jpg";
+// } 
+// if ( num === 3 ) {
+//     imageDOM.src = "images/art-therapy-4.jpg";
+// } 
+// if ( num === 4 ) {
+//     imageDOM.src = "images/art-therapy-5.jpeg";
+// }
 
+// num ++;
+// if (num ==5) {
+//     num = 1;
+// }
+// setTimeout(slideShow,3000);
 
-        $ ("#outreach").mouseenter(function(){
-            hovered = true;
-         
-        });
-            $("#outreach").mouseleave(function() {
-             setTimeout(function() {
-            $("#outreach").fadeOut(100);
-              $("#button-outreach").css("background-color","white");
+// }
 
-            hovered = false;
-            },500);
-            });
+// slideShow();
 
-        $("#button-outreach").mouseleave(function(){
-           
-            setTimeout(function(){
-                if (hovered === false) {
-                    console.log("false hover");
-                    $("#outreach").fadeOut(100);
-                      $("#button-outreach").css("background-color","white");
-
-                }
-            },1000);
-        });
-    
-    });
-
-    /*need to figure out how to pass arguments that select dom into jquery */
-//WIP
-// var NavBarred = function(item){
-//     $("#button-"+item).mouseenter(function(){
-//     var hovered = false;  
-//         $("#"+ item).fadeIn('slow');
-
-//         $ ("#" +item).mouseenter(function(){
-//             hovered = true;
-//             console.log(hovered);
-//         });
-//             $("#" +item).mouseleave(function() {
-//              setTimeout(function() {
-//             $("#"+item).fadeOut('slow');
-//             hovered = false;
-//             },500);
-//             });
-
-//         $("#button-" +item).mouseleave(function(){
-//             console.log("left " + hovered);
-//             setTimeout(function(){
-//                 if (hovered === false) {
-//                     console.log("false hover");
-//                     $("#"+item).fadeOut('slow');
-//                 }
-//             },1000);
-//         });
-    
-//     });
-// };
-// NavBarred(outreach);
 
 });
